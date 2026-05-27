009275fce2a5295ae45f3e5d94cc14760554da28
rishadislam1 <rishad.islam158@gmail.com>
2026-05-27 16:57:46 +0600

    Add range of data

---
+++ EFCoreDBOperationProject/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using EFCoreDBOperationProject.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreDBOperationProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController(AppDbContext appDbContext) : ControllerBase
+    {
+        [HttpPost("")]
+        public async Task<IActionResult> AddNewBook([FromBody] Books model)
+        {
+            appDbContext.Books.Add(model);
+            await appDbContext.SaveChangesAsync();
+            return Ok(model);
+        }
+
+        [HttpPost("bulk")]
+        public async Task<IActionResult> AddBooks([FromBody] List<Books> model)
+        {
+            appDbContext.Books.AddRange(model);
+            await appDbContext.SaveChangesAsync();
+            return Ok(model);
+        }
+    }
+}
